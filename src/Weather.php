0b9517affd0d4e96c5f3e74905c09541a508fb65
ajiho <lujiahao@88.com>
2024-07-25 15:51:38 +0800

    chore: 更新命名空间
    Signed-off-by: ajiho <lujiahao@88.com>

--- src/Weather.php
+++ src/Weather.php
@@ -1,6 +1,21 @@
 <?php
 
-class Weather {
+namespace Ajiho\Weather;
 
-    
+class Weather
+{
+     /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * Weather constructor.
+     *
+     * @param string $key
+     */
+    public function __construct($key)
+    {
+        $this->key = $key;
+    }
 }
